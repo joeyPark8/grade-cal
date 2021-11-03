db5b41d3396038f9613d070d010e98eb29d00edf
Joey Park <61180930+joeyPark8@users.noreply.github.com>
2021-11-03 16:11:14 +0900

    Add files via upload

--- bin/grade_cal.dart
+++ bin/grade_cal.dart
@@ -5,7 +5,7 @@ class GradeCal {
   
   List<List<String>>? table = [];
 
-  cal(int score, int average, int standardDeviation) async {
+  cal(int score, double average, double standardDeviation) async {
     await load();
 
     num z = (score - average) / standardDeviation;
@@ -15,20 +15,12 @@ class GradeCal {
     var z1 = double.parse((z < 0) ? str_z.substring(0,4) : str_z.substring(0,3));
     var z2 = double.parse(str_z.substring(str_z.length - 1));
 
-    // print(table);
-    //
-    // print(z);
-    // print(z1);
-    // print(z2);
-
     var index1 = int.parse('${(z1 + 4) * 10}'.split('.').first);
-    var index2 = int.parse('${z2 * 10}'.split('.').first);
+    var index2 = int.parse('${z2}'.split('.').first);
 
     var percentage = 100 - (double.parse(table![index1][index2]) * 100);
 
-    //print(percentage);
-
-    return percentage;
+    return double.parse(percentage.toStringAsFixed(3));
   }
 
   load() async {
@@ -47,4 +39,18 @@ class GradeCal {
         }
       );
   }
+}
+
+main() async {
+  var cal = GradeCal();
+
+  var korean = await cal.cal(96, 71.4, 23.9);
+  var history = await cal.cal(100, 72.0, 29.6);
+  var math = await cal.cal(100, 73.4, 29.3);
+  var english = await cal.cal(100, 59.7, 30.2);
+
+  print(korean);
+  print(history);
+  print(math);
+  print(english);
 }
